f8029e78b7e3f72f26d83542d500ac39f997e931
Thijs Coenen <info@thylacine.nl>
2018-04-11 15:55:39 +0200

    OP-135 remove duplicated definition

--- datapunt_api/rest.py
+++ datapunt_api/rest.py
@@ -1,13 +1,13 @@
 from collections import OrderedDict
 
-from rest_framework import pagination, response
 from rest_framework import renderers, serializers
 from rest_framework import viewsets, filters
 from rest_framework.reverse import reverse
-from rest_framework.utils.urls import replace_query_param
 from rest_framework_extensions.mixins import DetailSerializerMixin
 
 from django_filters.rest_framework import DjangoFilterBackend
+
+from .pagination import HALPagination
 
 DEFAULT_RENDERERS = (renderers.JSONRenderer, renderers.BrowsableAPIRenderer)
 FORMATS = [dict(format=r.format, type=r.media_type) for r in DEFAULT_RENDERERS]
@@ -47,41 +47,6 @@ class LinksField(serializers.HyperlinkedIdentityField):
 class HALSerializer(serializers.HyperlinkedModelSerializer):
     url_field_name = '_links'
     serializer_url_field = LinksField
-
-
-class HALPagination(pagination.PageNumberPagination):
-    """
-    Implement HAL-JSON style pagination (standard for Datapunt APIs).
-    """
-    page_size_query_param = 'page_size'
-
-    def get_paginated_response(self, data):
-        self_link = self.request.build_absolute_uri()
-        if self_link.endswith(".api"):
-            self_link = self_link[:-4]
-
-        if self.page.has_next():
-            next_link = replace_query_param(
-                self_link, self.page_query_param, self.page.next_page_number())
-        else:
-            next_link = None
-
-        if self.page.has_previous():
-            prev_link = replace_query_param(
-                self_link, self.page_query_param,
-                self.page.previous_page_number())
-        else:
-            prev_link = None
-
-        return response.Response(OrderedDict([
-            ('_links', OrderedDict([
-                ('self', dict(href=self_link)),
-                ('next', dict(href=next_link)),
-                ('previous', dict(href=prev_link)),
-            ])),
-            ('count', self.page.paginator.count),
-            ('results', data)
-        ]))
 
 
 class _DisabledHTMLFilterBackend(DjangoFilterBackend):
