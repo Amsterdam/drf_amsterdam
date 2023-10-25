6e7785c2e62a70943bbdb260a5ffab4847c52ed5
Youri Westerman <y.westerman@amsterdam.nl>
2023-10-25 12:07:47 +0200

    Match arguments of parent and add types

--- datapunt_api/renderers.py
+++ datapunt_api/renderers.py
@@ -1,10 +1,19 @@
+from typing import Any
+
 from rest_framework_csv.renderers import CSVRenderer
 
 
 class PaginatedCSVRenderer(CSVRenderer):
-    results_field = 'results'
+    results_field: str = 'results'
 
-    def render(self, data, *args, **kwargs):
+    def render(
+            self,
+            data: dict | list,
+            media_type: str | None = None,
+            renderer_context: dict[str, Any] = {},
+            writer_opts: dict[str, Any] | None = None,
+    ) -> str | bytes:
         if not isinstance(data, list):
             data = data.get(self.results_field, [])
-        return super(PaginatedCSVRenderer, self).render(data, *args, **kwargs)
+
+        return super(PaginatedCSVRenderer, self).render(data, media_type, renderer_context, writer_opts)
